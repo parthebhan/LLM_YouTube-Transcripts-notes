315dda18c161d1d536a0557497da871879b1c47b
Parthebhan <parthebhan143@gmail.com>
2024-08-20 20:52:21 +0530

    App with Download option

--- streamlit.py
+++ streamlit.py
@@ -31,10 +31,8 @@ def extract_transcript_details(youtube_video_url):
         if not video_id:
             raise ValueError("Invalid YouTube URL")
         transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
-        
         transcript = " ".join(item["text"] for item in transcript_text)
         return transcript
-
     except Exception as e:
         st.error(f"An error occurred while extracting the transcript: {e}")
         return None
@@ -53,11 +51,7 @@ def generate_gemini_content(transcript_text, prompt):
 def generate_docx(summary):
     doc = Document()
     doc.add_heading('Detailed Notes', level=1)
-
-    # Convert Markdown to HTML
     html_summary = markdown(summary)
-
-    # Parse HTML and add formatted text to DOCX
     soup = BeautifulSoup(html_summary, 'html.parser')
 
     for element in soup:
@@ -75,7 +69,6 @@ def generate_docx(summary):
             for li in element.find_all('li'):
                 doc.add_paragraph(li.text, style='ListBullet')
 
-    # Save the DOCX to a BytesIO object
     docx_output = BytesIO()
     doc.save(docx_output)
     docx_output.seek(0)
@@ -92,11 +85,19 @@ def get_video_id(url):
 st.markdown("""
     <h1 style='font-size: 32px;'>Transform YouTube 📹 Transcripts into Impactful Detailed Notes</h1>
     """, unsafe_allow_html=True)
-youtube_link = st.text_input("Enter YouTube Video Link:")
 
-# Initialize summary and DOCX file variables
-summary = None
-docx_file = None
+# Initialize or clear session state
+if 'summary' not in st.session_state:
+    st.session_state.summary = None
+if 'docx_file' not in st.session_state:
+    st.session_state.docx_file = None
+
+def reset_app():
+    st.session_state.summary = None
+    st.session_state.docx_file = None
+    st.rerun()
+
+youtube_link = st.text_input("Enter YouTube Video Link:")
 
 if youtube_link:
     video_id = get_video_id(youtube_link)
@@ -108,14 +109,14 @@ if youtube_link:
             transcript_text = extract_transcript_details(youtube_link)
 
             if transcript_text:
-                summary = generate_gemini_content(transcript_text, prompt)
+                st.session_state.summary = generate_gemini_content(transcript_text, prompt)
                 
-                if summary:
+                if st.session_state.summary:
                     st.markdown("## Detailed Notes:")
-                    st.write(summary)
+                    st.write(st.session_state.summary)
 
                     # Generate DOCX
-                    docx_file = generate_docx(summary)
+                    st.session_state.docx_file = generate_docx(st.session_state.summary)
                     
                 else:
                     st.error("Failed to generate summary.")
@@ -127,16 +128,26 @@ st.sidebar.markdown("Enter the YouTube Video Link and Click Get Detailed Notes b
 st.sidebar.title("DOWNLOAD MENU")
 
 with st.sidebar:
-    if docx_file:
-        st.markdown("Summary Generated Sucessfully")
+    if st.session_state.docx_file:
+        st.markdown("Summary Generated Successfully !!!")
         st.write(" ")
         st.info("Click the button below to download your detailed notes in DOCX format.")
         st.download_button(
             label="Download Summary and Exit",
-            data=docx_file,
+            data=st.session_state.docx_file,
             file_name="summary.docx",
             mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
         )
     else:
         st.write("No summary available for download.")
 
+
+st.sidebar.markdown("""
+    <h1 style='font-size: 20px;'>Manage Reset</h1>""", unsafe_allow_html=True)
+if st.sidebar.button("Reset"):
+    
+    reset_app()
+
+ # Add the credit section
+st.sidebar.markdown("<hr>", unsafe_allow_html=True)  # Adds a horizontal line with HTML
+st.sidebar.markdown("<h3 style='color: #2ca02c;font-size: 20px;'>App Created by: Parthebhan Pari</h3>", unsafe_allow_html=True)
